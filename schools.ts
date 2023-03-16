db39966ee803baf3b402ee6ebcf19c19a8cebd5a
Anand <anand@dev-bee.com>
2023-03-16 12:33:48 +0530

    <>|| url changed for islamiah school||<anand>

--- schools.ts
+++ schools.ts
@@ -3,6 +3,6 @@ export const schoolsList = [
     Id: 1,
     Name: "Islamiyah matriculation higher secondry school, coimbatore",
     ShortName: "Islamiyah School",
-    Url: "http://localhost/smart-school/api/",
+    Url: "http://dev-bee.com/devbee-school/api/",
   },
 ];
